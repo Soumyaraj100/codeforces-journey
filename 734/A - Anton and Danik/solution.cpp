d8269907069420258f7c00337d5538c790f8c732
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-08-16 23:58:42 +0530

    Add Anton and Danik [A] from Codeforces

---
+++ 734/A - Anton and Danik/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    int a = 0, d = 0;
+    for (char x : s) {
+        if (x == 'A')
+            a++;
+        else
+            d++;
+    }
+    if (a > d)
+        cout << "Anton";
+    else if (d > a)
+        cout << "Danik";
+    else
+        cout << "Friendship";
+    return 0;
+}
