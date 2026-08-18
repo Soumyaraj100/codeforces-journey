5ec68cfbbe24dcc9b20b9df29005c45cf7293bef
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-08-18 23:57:59 +0530

    Add Vanya and Fence [A] from Codeforces

---
+++ 677/A - Vanya and Fence/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, h;
+    cin >> n >> h;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if (a > h)
+            ans += 2;
+        else
+            ans += 1;
+    }
+    cout << ans;
+    return 0;
+}
