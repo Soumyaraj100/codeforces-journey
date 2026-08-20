9721c5a46b2991888f9f089ffcb1c834885bf851
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-08-20 14:17:23 +0530

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n, b;
+    double s = 0;
+ 
+    cin >> n;
+ 
+    for(int i = 0; i < n; i++) {
+        cin >> b;
+        s += b;
+    }
+ 
+    cout << s / n;
+ 
+    return 0;
+}
